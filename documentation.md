7acb02905a82a98bbeebee146028f9fa6867ceac
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-06-01 14:39:49 +0200

    Domains: WIP

--- documentation.md
+++ documentation.md
@@ -401,7 +401,7 @@ and it's the default value.
 * any nice value *N* lesser than 0 will emit event synchronously until *-N* recursion is reached, after that, setImmediate()
   will be used to call the listeners, the first event count as 1 recursion, so if nice=-1, all events will be asynchronously emitted,
   if nice=-2 the initial event will call the listener synchronously, but if the listener emits events on the same emitter object,
-  the sub-listener will be called through setTimeout(), breaking the recursion... and so on...
+  the sub-listener will be called through setImmediate(), breaking the recursion... and so on...
 
 They are many elements that can define their own *nice value*.
 
--- test/.tea-time/NextGenEvents-test.js
+++ test/.tea-time/NextGenEvents-test.js
@@ -1,21 +1,21 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*
-	The Cedric's Swiss Knife (CSK) - CSK NextGen Events
+	Next Gen Events
 	
-	Copyright (c) 2015 Cédric Ronvel 
+	Copyright (c) 2015 - 2016 Cédric Ronvel
 	
 	The MIT License (MIT)
-
+	
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-
+	
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-
+	
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -727,20 +727,22 @@ NextGenEvents.processQueue = function processQueue( contextName , isCompletionCa
 
 },{}],2:[function(require,module,exports){
 /*
-	Copyright (c) 2015 Cédric Ronvel 
+	Next Gen Events
+	
+	Copyright (c) 2015 - 2016 Cédric Ronvel
 	
 	The MIT License (MIT)
-
+	
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-
+	
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-
+	
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -2057,22 +2059,22 @@ module.exports.isBrowser = true ;
 },{"buffer":5}],4:[function(require,module,exports){
 (function (process){
 /*
-	The Cedric's Swiss Knife (CSK) - CSK NextGen Events
-
-	Copyright (c) 2015 Cédric Ronvel 
+	Next Gen Events
+	
+	Copyright (c) 2015 - 2016 Cédric Ronvel
 	
 	The MIT License (MIT)
-
+	
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-
+	
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-
+	
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
